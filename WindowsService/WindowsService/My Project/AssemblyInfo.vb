920efa9eebfca5294ddf8d5f2142e8f8fecdc2eb
Dhananjay Waikar <dhananjay.waikar@ics-global.in>
2022-05-10 13:14:54 +0530

    Added Windows Service Project

---
+++ WindowsService/WindowsService/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WindowsService")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WindowsService")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5b860cb6-c71f-4c82-b25f-11bbd5c8ba9c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ WindowsService/WindowsService/Service1.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Configuration
+Imports System.Threading
+Imports System.ServiceProcess
+Imports System.IO
+
+Public Class Service1
+
+    Dim ScheduleSleepTime As Integer = Convert.ToInt32(ConfigurationSettings.AppSettings("ThreadSleepTimeInMin"))
+    Dim worker As Thread = Nothing
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        ' Add code here to start your service. This method should set things
+        ' in motion so your service can do its work.
+        Try
+            Dim start As ThreadStart = New ThreadStart(AddressOf Working)
+            worker = New Thread(start)
+            worker.Start()
+        Catch ex As Exception
+            Throw
+        End Try
+
+
+    End Sub
+    Public Sub Working()
+        While (True)
+            Dim path As String = "D:\\sample.txt"
+            Using writer As StreamWriter = New StreamWriter(path, True)
+                writer.WriteLine(String.Format("Windows Service Called on " & DateTime.Now.ToString("dd /MM/yyyy hh:mm:ss tt")))
+                writer.Close()
+            End Using
+            Thread.Sleep(ScheduleSleepTime * 60 * 1000)
+        End While
+    End Sub
+
+    Protected Overrides Sub OnStop()
+        ' Add code here to perform any tear-down necessary to stop your service.
+        Try
+            If (worker IsNot Nothing) And worker.IsAlive Then
+                worker.Abort()
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+End Class
